c7cd1e29160accc28c8d4b4802bbef1d1f6d6ade
allenwhale <allenwhale@yahoo.com.tw>
2015-05-22 01:37:50 +0800

    admin1

--- backend/admin.py
+++ backend/admin.py
@@ -8,6 +8,36 @@ class AdminService:
         self.db = db
         AdminService.inst = self
 
+    def update_admin1(self, acct, data):
+        def gen_sql(data):
+            first = True
+            sql = ' SET '
+            prama = ()
+            for i in data:
+                if first == False:
+                    sql += ','
+                first = False
+                sql += '"%s" = '%i
+                sql += '%s '
+                prama = prama + (data[i],)
+            return (sql, prama)
+
+        if not acct or acct['admin'] == 0:
+            return ('Eaccess', None)
+
+        cur = yield self.db.cursor()
+        uid = data['uid']
+        data.pop('uid')
+        sql, prama = gen_sql(data)
+        print('sql', sql)
+        print('prama', prama)
+        print('uid', uid)
+        yield cur.execute('UPDATE "account_info" '+sql+' WHERE "uid" = %s;', prama + (uid,))
+        print(cur.rowcount)
+        if cur.rowcount != 1:
+            return ('Edb', None)
+        return (None, uid)
+
     def update_admin3(self, acct, data, flag_img):
         if not acct or acct['admin'] == 0:
             return ('Eaccess', None)
@@ -37,8 +67,16 @@ class AdminHandler(RequestHandler):
     @reqenv
     def post(self):
         req = self.get_argument('req', None)
+        print(req)
         if req == 'admin1':
-            pass
+            args = ['uid', 'preference', 'country']
+            meta = self.get_args(args)
+            err, uid = yield from AdminService.inst.update_admin1(self.acct, meta)
+            if err:
+                self.finish(err)
+                return
+            self.finish('S')
+            return
         elif req == 'admin2':
             pass
         elif req == 'admin3':
