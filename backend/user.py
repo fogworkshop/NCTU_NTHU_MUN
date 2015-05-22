6151b52eba207ae72e29899bba869a86392ed027
allenwhale <allenwhale@yahoo.com.tw>
2015-05-23 02:52:40 +0800

    upload

--- backend/user.py
+++ backend/user.py
@@ -116,7 +116,6 @@ class UserService:
         uid = meta['uid']
         sub = subprocess.Popen('find ../http/'+str(uid)+' | grep flag', shell=True, stdout=subprocess.PIPE)
         sub = sub.communicate()[0].decode()
-
         if sub != '' and sub[-1] == '\n':
             sub = sub[:-1]
         if sub != '':
